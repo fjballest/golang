ce3e1a91b55777461c56d9f925f28d5b3a967230
Nemo <nemo.mbox@gmail.com>
2016-02-12 11:53:45 +0100

    guide

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 unset GOROOT GOENV GOPATH GOBIN GORBIN
-GOROOT=/usr/local/go
+GOROOT=`pwd`
 GOROOT_BOOTSTRAP=/usr/local/go14
 PATH=$GOROOT/bin:$PATH
 export GOROOT path GOROOT_BOOTSTRAP
-cd /usr/local/go/src
+cd src
 ./all.bash
 
